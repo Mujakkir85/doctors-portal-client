80873abfe63fd217dccb80e6d147642ad525f366
Mujakkir85 <mujakkirlucse@gmail.com>
2022-05-11 19:50:03 +0600

    add modal and end of module 72

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -8,7 +8,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cartTitle="Opening hours" img={clock}></InfoCard>
-            <InfoCard bgClass="bg-accent" cartTitle="Our Locations" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-[#3A4256]" cartTitle="Our Locations" img={marker}></InfoCard>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cartTitle="Contact Us" img={phone}></InfoCard>
         </div>
     );
