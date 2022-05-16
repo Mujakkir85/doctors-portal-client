d171fcf0e1992feeea264f01261267d107d3dbb5
Mujakkir85 <mujakkirlucse@gmail.com>
2022-05-17 00:29:21 +0600

    added amdin and all user

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
+const UserRow = ({ user, refetch }) => {
 
-    const { email } = user;
+    const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to Make an admin');
+                }
+                return res.json()
+            })
+            .then(data => {
+                //console.log(data)
+                if (data.modifiedCount > 0) {
+                    refetch(); // for show change ui without reload
+                    toast.success(`Successfully made an admin`);
+                }
+            })
+    }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
+            <td><button className="btn btn-xs">Remove user</button></td>
         </tr>
     );
 };
